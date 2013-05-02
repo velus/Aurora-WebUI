12123c3b8128cf947f13f6cd05ec99db46f3842d
velus <admin@velus.co.uk>
2013-05-02 13:05:57 -0500

    just an major updat

--- www/sites/main/worldmap.php
+++ www/sites/main/worldmap.php
@@ -1,27 +1,22 @@
-<html lang="en">
-<head>
-<meta charset="utf-8">
-    <title><?= SYSNAME ?>: <? echo $webui_world_map ?></title>
-    <meta name="keywords" content="{SystemName}" />
-    <meta name="description" content="{SystemName}"/>
-    <link rel="shortcut icon" href="images/icons/favicon.ico" />
-    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key="></script>
-    <script type="text/javascript" src="javascripts/jquery/jquery.min.js"></script>
-    <script src="map/slmapapi.php"></script>
-    <link rel="stylesheet" type="text/css" href="map/slmapapi.css" />
+<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key="></script>
+<script src="worldmap/slmapapi.php"></script>
+<link rel="stylesheet" type="text/css" href="worldmap/css/slmapapi.css" />
+<style type="text/css">#map-container {width: 100%; height: 380px; color: #546368;}</style>
 
-    <style type="text/css">
-	    html, body, {width: 100%; height: 100%; margin: 0px; padding: 0px;}
-		#map-container {width: 100%; height: 445px;}
-    </style>
-
-    <script type="text/javascript">
+<script type="text/javascript">
     function loadmap(){
-        var coords = {'x' : <?  echo $webui_map_GridCenterX ?> + 0.5, 'y' : <? echo $webui_map_GridCenterY ?> + 0.5},
+        var coords = {'x' : <?php echo $mapstartX; ?> + 0.5, 'y' : <?php echo $mapstartY; ?> + 0.5},
         mapInstance = new SLURL.Map(document.getElementById('map-container'), {'overviewMapControl':true});
         mapInstance.centerAndZoomAtSLCoord(new SLURL.XYPoint(coords.x, coords.y), 3);}
     $(document).ready(loadmap);
-    </script>
-</head>
+</script>
 
-<body><div id=map-container></div></body></html>
+<div id="content">
+    <div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?>: <?php echo $webui_world_map; ?></h5></div>
+    <div id="ContentHeaderCenter"></div>
+    <div id="ContentHeaderRight">
+        <h5><a onClick="window.open('<?php echo SYSURL; ?>worldmap/','mywindow')" style="cursor:pointer; float:right; display:inline-block;"><?php echo $webui_fullscreen; ?></a></h5>
+    </div>
+    <div class="clear"></div>
+    <div id = "map-container"></div>
+</div>
