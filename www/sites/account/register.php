2dc5178578a98e49380ec6c490be15296b5dbe64
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2013-05-19 01:01:10 +0100

    removed un-needed html code from registration.php and notes on security

--- www/sites/account/register.php
+++ www/sites/account/register.php
@@ -84,7 +84,8 @@ $terms_of_service = "I need to import this...";
 $smarty->assign("terms_of_service", $terms_of_service);
 
 // Get IP Adress
-if ($_SERVER["HTTP_X_FORWARDED_FOR"]) {
+//XXX since we get HTTP_X_FORWARDED_FOR from the browser it's self, I could set my IP address to "bugs bunny" and the code would be none the wiser.
+if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
     $userIP = $_SERVER["HTTP_X_FORWARDED_FOR"];
 } elseif ($_SERVER["REMOTE_ADDR"]) {
     $userIP = $_SERVER["REMOTE_ADDR"];
@@ -94,75 +95,14 @@ if ($_SERVER["HTTP_X_FORWARDED_FOR"]) {
 
 return;
 
+//XXX below this point, there be dragons. (okay, this code just needs to be refactored out of existance, but dragons is more punchy.)
+
 if($ALLOWREGISTRATION == '1')
 {
 
 if ($_POST[action] == "") {
     $_SESSION[PASSWD] = "";
     $_SESSION[EMAIC] = "";
-	
-
-function displayCountry()
-{
-	$DbLink = new DB;
-	echo "<div class=\"roundedinput\"><select require=\"true\" label=\"country_label\" id=\"register_input\" wide=\"25\" name=\"country\" value=\"$_SESSION[COUNTRY]\">";
-	$DbLink->query("SELECT name FROM " . C_COUNTRY_TBL . " ORDER BY name ASC ");
-	echo "<option></option>";
-	while (list($COUNTRYDB) = $DbLink->next_record()) {
-		echo "<option>$COUNTRYDB</option>";
-	}
-	echo "</select></div>";
-}
-
-
-function displayDOB()
-{	
-	echo "<div id=\"birthday\" class=\"roundedinput\"><table><tr><td>";
-	 
-	if ($status == 1 and $monat == '')
-		echo "<select label=\"dob_label\" id=\"birthday_input\" require=\"true\" name='tag' class='red'>"; 
-	else
-		echo "<select label=\"dob_label\" id=\"birthday_input\" require=\"true\" name='tag' class='black'>"; 
-	echo "<option></option>";
-	for ($i = 1; $i <= 31; $i++) 
-	{
-		echo("<OPTION VALUE=\"$i\" ");
-		if ($tag == $i)
-			echo("selected ");
-		echo(">$i");
-	}
-	echo "</select>";
-
-	if ($status == 1 and $monat == '')
-		echo "<select label=\"dob_label\" id=\"birthday_input\" require=\"true\" name='monat' class='red'>"; 
-	else
-		echo "<select label=\"dob_label\" id=\"birthday_input\" require=\"true\" name='monat' class='black'>"; 
-	echo "<option></option>";
-	for ($i = 1; $i <= 12; $i++) 
-	{
-		echo("<OPTION VALUE=\"$i\" ");
-		if ($monat == $i)
-			echo("selected ");
-		echo(">$i");
-	}
-	echo "</select>";
-	if ($status == 1 and $jahr == '')
-		echo "<select label=\"dob_label\" id=\"birthday_input\" require=\"true\" name='jahr' class='red'>"; 
-	else
-		echo "<select label=\"dob_label\" id=\"birthday_input\" require=\"true\" name='jahr' class='black'>"; 
-	echo "<option></option>";
-	$jetzt = getdate();
-	$jahr1 = $jetzt["year"];
-
-	for ($i = 1920; $i <= $jahr1; $i++) {
-		echo("<OPTION VALUE=\"$i\" ");
-		if ($jahr == $i)
-			echo("selected ");
-		echo(">$i");
-	}
-	echo "</select></td></tr></table></div>";
-}
-
 
 function displayDefaultAvatars()
 {
@@ -191,195 +131,7 @@ function displayDefaultAvatars()
 	}
 }
 
-return;
-?>
-
-    <div id="content">
-    
-    <div id="ContentHeaderLeft"><h5><?php echo SYSNAME; ?></h5></div>
-    <div id="ContentHeaderCenter"></div>
-    <div id="ContentHeaderRight"><h5><?php echo $webui_register; ?></h5></div>
-	
-    <div id="register">
-        <form ACTION="index.php?page=register" METHOD="POST" onsubmit="if (!validate(this)) return false;">
-            <table>
-                <tr><td class="error" colspan="2" align="center" id="error_message"><?php echo $_SESSION[ERROR]; ?><?php echo $_GET[ERROR]; ?></td></tr>
-                <tr>
-                    <td class="even" width="52%"><span id="accountfirst_label"><?php echo $webui_avatar_first_name; ?>*</span></td>
-                    <td class="even">
-                        <div class="roundedinput">
-                            <input minlength="3" id="register_input" require="true" nospecial="true" label="accountfirst_label" name="accountfirst" type="text" size="25" maxlength="15" value="<?php echo $_SESSION[ACCFIRST]; ?>">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="odd"><span id="accountlast_label"><?php echo $webui_avatar_last_name; ?>*</span></td>
-                    <td class="odd">
-                        <?php echo printLastNames(); ?>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="even"><span id="wordpass_label"><?php echo $webui_password; ?>*</span></td>
-                    <td class="even">
-                        <div class="roundedinput">
-                            <input minlength="6" compare="wordpass2" require="true" nospecial="true" label="wordpass_label" id="register_input" name="wordpass" type="password" size="25" maxlength="15">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="odd"><span id="wordpass2_label"><?php echo $webui_confirm; ?> <?php echo $webui_password; ?>*</span></td>
-                    <td class="odd">
-                        <div class="roundedinput">
-                            <input minlength="6" require="true" nospecial="true" label="wordpass2_label" id="register_input" name="wordpass2" type="password" size="25" maxlength="15">
-                        </div>
-                    </td>
-                </tr>
-
-                <?php if ($REGIOCHECK == "0") { ?>
-
-                <tr>
-                    <td class="even"><span id="startregion_label"><?php echo $webui_start_region; ?>*</span></td>
-                    <td class="even">
-                        <?php echo displayRegions(); ?>
-                    </td>
-                </tr>
-
-                <?php } if ($ADRESSCHECK == "1") { ?>
-                <tr>
-                    <td class="odd"><span id="firstname_label"><?php echo $webui_first_name; ?>*</span></td>
-                    <td class="odd">
-                        <div class="roundedinput">
-                            <input require="true" nospecial="true" label="firstname_label" id="register_input" name="firstname" type="text" size="25" maxlength="15" value="<?php echo $_SESSION[NAMEF]; ?>">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="even"><span id="lastname_label"><?php echo $webui_last_name; ?>*</span></td>
-                    <td class="even">
-                        <div class="roundedinput">
-                            <input require="true" nospecial="true" label="lastname_label" id="register_input" name="lastname" type="text" size="25" maxlength="15" value="<?php echo $_SESSION[NAMEL]; ?>">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="odd"><span id="adress_label"><?php echo $webui_address; ?>*</span></td>
-                    <td class="odd">
-                        <div class="roundedinput">
-                            <input require="true" nospecial="true" label="adress_label" id="register_input" name="adress" type="text" size="50" maxlength="50" value="<?php echo $_SESSION[ADRESS]; ?>">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="even"><span id="zip_label"><?php echo $webui_zip_code; ?>*</span></td>
-                    <td class="even">
-                        <div class="roundedinput">
-                            <input require="true" nospecial="true" label="zip_label" id="register_input" name="zip" type="text" size="25" maxlength="15" value="<?php echo $_SESSION[ZIP]; ?>">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="odd"><span id="city_label"><?php echo $webui_city; ?>*</span></td>
-                    <td class="odd">
-                        <div class="roundedinput">
-                            <input require="true" nospecial="true" label="city_label" id="register_input" name="city" type="text" size="25" maxlength="15" value="<?php echo $_SESSION[CITY]; ?>">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="even"><span id="country_label"><?php echo $webui_country; ?>*</span></td>
-                    <td class="even">
-                        <?php echo displayCountry(); ?>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="odd"><span id="dob_label"><?php echo $webui_date_of_birth; ?>*</span></td>
-                    <td class="odd">
-                        <?php echo displayDOB(); ?>
-                    </td>
-                </tr>
-
-                <?php } else if ($FORCEAGE == "1"){ ?>
-
-                <tr>
-                    <td class="odd"><span id="dob_label"><?php echo $webui_date_of_birth; ?>*</span></td>
-                    <td class="odd">
-                        <?php echo displayDOB(); ?>
-                    </td>
-                </tr>
-
-                <?php } ?>
-
-                <tr>
-                    <td class="odd"><span id="email_label"><?php echo $webui_email; ?>*</span></td>
-                    <td class="odd">
-                        <div class="roundedinput">
-                            <input compare="emaic" require="true" nospecial="true" label="email_label" id="register_input" name="email" type="text" size="40" maxlength="40" value="<?php echo $_SESSION[EMAIL]; ?>">
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td class="even"><span id="emaic_label"><?php echo $webui_confirm; ?> <?php echo $webui_email; ?>*</span></td>
-                    <td class="even">
-                        <div class="roundedinput">
-                            <input require="true" nospecial="true" label="emaic_label" id="register_input" name="emaic" type="text" size="40" maxlength="40" value="<?php echo $_SESSION[EMAIC]; ?>" >
-                        </div>
-                    </td>
-                </tr>
-
-                <?php displayDefaultAvatars(); ?>
-
-                <?php if( file_exists( $_SERVER{'DOCUMENT_ROOT'} . "/TOS.php"))  { ?>
-
-                <tr>
-                    <td class="even" colspan="2">
-                        <div style="width:100%;height:300px;overflow:auto;">
-                            <?php include("tos.php"); ?>
-                        </div>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td colspan="2" valign="top" class="odd"><input label="agree_label" require="true" type="checkbox" name="Agree_with_TOS" id="agree" value="1" />
-                        <label for="agree"><span id="agree_label"><?php echo $site_terms_of_service_agree; ?></span></label>
-                    </td>
-                </tr>
-
-                <?php } ?>
-
-                <tr>
-                    <td class="even">
-                        <div class="center">
-                            <?php echo "<script type=\"text/javascript\">var RecaptchaOptions = {theme : '".$template_captcha_color."'};</script>"; ?>
-                            <?php require_once('recaptchalib.php');
-                                $publickey = "6Lf_MQQAAAAAAIGLMWXfw2LWbJglGnvEdEA8fWqk"; // you got this from the signup page
-                                echo recaptcha_get_html($publickey); ?>
-                        </div>
-                    </td>
-
-                    <td class="even">
-                        <div class="center">
-                            <input type="hidden" name="action" value="check">
-                            <button id="register_bouton" name="submit" type="submit"><? echo $webui_create_new_account ?></button>
-                            <!-- <input id="register_bouton" name="submit" TYPE="submit" value='<? //echo $webui_create_new_account ?>'> -->
-                        </div>
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </div>
-</div>
-
-<?php } else if ($_POST[action] == "check") 
+} else if ($_POST[action] == "check") 
 {
 	return;
 	$_SESSION[ACCFIRST] = $_POST[accountfirst];
